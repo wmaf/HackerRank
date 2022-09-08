2cf94ad4f8bef1fa89790e50837b0db0ce8de7e3
wmaf <wmaf@outlook.com>
2022-09-08 19:57:48 -0400

    Initial commit  Hacker Rank code

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,81 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        void positiveNegativeZero(int arr[], int len)
+        {
+            {
+                // Initialize the postiveCount, negativeCount, and
+                // zeroCountby 0 which will count the total number
+                // of positive, negative and zero elements
+                float positiveCount = 0;
+                float negativeCount = 0;
+                float zeroCount = 0;
+
+                // Traverse the array and count the total number of
+                // positive, negative, and zero elements.
+                for (int i = 0; i < len; i++) {
+                    if (arr[i] > 0) {
+                        positiveCount++;
+                    }
+                    else if (arr[i] < 0) {
+                        negativeCount++;
+                    }
+                    else if (arr[i] == 0) {
+                        zeroCount++;
+                    }
+                }
+                System.out.printf("%.6f", value);
+
+                static void miniMaxSum(int[] arr) {
+
+                int min = arr[0];
+                int max = arr[0];22
+                int totaSum = 0;
+                for (int n:arr) {
+                    totalSum +=n
+                    if (n<min) min = n
+                    if (n<max) max = n
+                }
+                sout.. (totalSum-max)  + "  "+  (totalMin - min)
+
+                SELECT NAME FROM CITY WHERE COUNTRYCODE = "USA" AND POPULATION > 1000000;
+
+                SELECT * FROM CITY
+                SELECT ID FROM CITY WHERE ID = 1661
+
+                meFirst
+                return a+b;
+
+                find the sum of its elements.
+                public static int simpleArraySum(List<Integer> ar) {
+
+                    int sum=0;
+                    for(int i=0; i<ar.size(); i++)
+                    {
+                        sum += ar.get(i);
+                    }
+                    return sum;
+                }
+            }
+
+                Alice and Bob each created one problem for HackerRank
+                public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+                int Alice = 0;
+                int Bob = 0;
+                int[] myIntArray = new int[2];
+                for (int i = 0; i <a.size(); i++) {
+                    for (int j = 0; j <b.size(); j++) {
+                        if (a.get(i)>b.get(j)){
+                            Alice ++;
+                            if (a.get(i)<b.get(j)) {
+                                Bob++;
+                                //if(a.get(i)==b.get(j)){
+
+                            }
+                        }
+                        return [Alice,Bob];
+                    }
+                }
+}
